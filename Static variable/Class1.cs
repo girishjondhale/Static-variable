99d4cab01e4eaca2941701f05604c91d0aa4799f
girishjondhale <girishjondhale1997@gmail.com>
2023-08-16 17:38:14 -0700

    Add project files.

---
+++ Static variable/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_variable
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Emlpoyee emp2 = new Emlpoyee("aaa", 14000);
+            emp2.CalculateSalary();
+            Console.WriteLine(emp2.CalculateSalary());
+
+            Console.WriteLine(Emlpoyee.Getcount());
+
+            Sales_maneger sales_Maneger = new Sales_maneger(500, 1, "ccc");
+            sales_Maneger.CalculateSalary();
+            Console.WriteLine(sales_Maneger);
+        }
+    }
+}
